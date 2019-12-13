f94bc5779a1a341f716002fe17a95656e30a92d2
Zachary Skluzacek <zachary.skluzacek@concordusa.com>
2019-12-13 16:55:50 -0600

    initial commit

---
+++ src/test/java/com/myretail/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myretail.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
